9b2000cc85c0cebd86bd476c745bb0e8fd48e59e
aroradhruv03 <aroradhruv03@gmail.com>
2016-04-26 23:46:27 -0500

    My Solutions

--- HackerRank/Java/BigNumber/BigDeciml.java
+++ HackerRank/Java/BigNumber/BigDeciml.java
@@ -2,6 +2,7 @@ package Java.BigNumber;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
 
@@ -16,41 +17,29 @@ public class BigDeciml {
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
         String []s=new String[n];
-//        BigDecimal bd[] = new BigDecimal[n];
+        //BigDecimal bd[] = new BigDecimal[n];
         for(int i=0;i<n;i++)
         {
             s[i] = sc.next();
         }
-//        for(int i=n-1;i>=0;i--)
-//        {
-//            System.out.println(s[i]);
-//        }
 
         //Write your code here
-
-//        Arrays.sort( s, (as,bs) -> {BigDecimal bd = new BigDecimal(bs);
-//            return bd.compareTo(new BigDecimal(as));} );
-
-        Arrays.sort(s, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                BigDecimal b1 =  new BigDecimal(o1);
-                BigDecimal b2 =  new BigDecimal(o2);
-//        if(b1==b2)
-//            return 0;
-                return b1.compareTo(b2);
-            }
-        });
-
-        
+        Arrays.sort(s, Collections.reverseOrder(new customComparator()));
 
 
         //Output
-        for(int i=n-1;i>=0;i--)
+        for(int i=0;i<n;i++)
         {
             System.out.println(s[i]);
         }
 
     }
 
+}
+
+class customComparator implements Comparator<String>{
+    @Override
+    public int compare(String o1, String o2) {
+        return new BigDecimal(o1).compareTo(new BigDecimal(o2));
+    }
 }
